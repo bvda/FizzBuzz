942e2b26b1b8a88f8d6e992b193a70a894b8446b
Henrik Kirk <henrikkirk@gmail.com>
2021-10-28 13:40:46 +0200

    Added ResultCollectorThread to FizzBuzz

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FizzBuzz
+﻿using System.Threading;
+namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var fb = new FizzBuzz();
+            var event_1 = new AutoResetEvent(true);
+            var event_2 = new AutoResetEvent(false);
+            var resultCollector = new ResultCollector(event_1, event_2);
+            var t = new Thread(resultCollector.Run);
+            t.Start();
+            var fb = new FizzBuzz(resultCollector, event_1, event_2);
             var result = fb.Calculate(start: 0, end: 1000, fizz: 7, buzz: 9);
             fb.WriteToFile(result);
         }
