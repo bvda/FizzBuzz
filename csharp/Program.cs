ec0e6c1d7cd449d04e0829590a99e1d3e3edd7a2
Henrik Kirk <henrikkirk@gmail.com>
2022-10-13 13:52:40 +0200

    Moved to csharp folder Change to for loop removed interface explicit variables

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fb = new FizzBuzz();
+            IEnumerable<string> result = fb.Calculate(0, 100);         
+            fb.WriteToFile(result);
+        }
+    }
+}
